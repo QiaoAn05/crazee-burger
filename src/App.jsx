efd92847498dcfbe6ed6852a3e5f265356d52dbe
Johan <johan.perez05@gmail.com>
2024-07-13 15:42:05 +0200

    feature start

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useState } from "react"
+
 
 function App() {
   //States
- const [prenom, setPrenom] =  useState("Johan")
- const [count, setCount] = useState(1)
- 
-  //Comportements
 
-  const handleClick = () => {
-    setCount(count+1)
-  }
+  //Comportements
 
   //Render
   return (
     <div>
-      <h1>Bonjour {prenom}</h1>
-      <p>{count}</p>
-      <button onClick={handleClick}>Incrémenter</button>
+
     </div>
   )
 }
