76de8f5453ccf2b8a54493310cdfcec63541d9e1
Johan <johan.perez05@gmail.com>
2024-07-14 16:05:35 +0200

    ✨ F01: LOGIN > Exchange content App.jsx to LoginPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
-import { useState } from "react"
 import LoginPage from "./components/pages/LoginPage"
 
 function App() {
-  //States
-  const [firstname, setFirstname] = useState("")
-
-  //Comportements
-  const handleChange = (e) => {
-    setFirstname(e.target.value)
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    alert("Bonjour " + firstname)
-    setFirstname("")
-  }
-
-  //Render
   return (
-    <div>
       <LoginPage/>
-      <h1>Bienvenue chez nous !</h1>
-      <h2>Connectez-vous</h2>
-      <form action="submit" onSubmit={handleSubmit}>
-        <input
-          value={firstname} 
-          onChange={handleChange} 
-          type="text" 
-          placeholder="Entrez votre prénom" 
-          required
-        />
-        <button>Accéder à votre espace</button>
-      </form>
-    </div>
   )
 }
 
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+  //States
+  const [firstname, setFirstname] = useState("")
+
+  //Comportements
+  const handleChange = (e) => {
+    setFirstname(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert("Bonjour " + firstname)
+    setFirstname("")
+  }
+
+  //Render
   return (
-    <div>LoginPage</div>
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={firstname} 
+          onChange={handleChange} 
+          type="text" 
+          placeholder="Entrez votre prénom" 
+          required
+        />
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
   )
 }
