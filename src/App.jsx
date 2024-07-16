c3a853783d7db4a896df432b956a47e83734c4ac
Johan <johan.perez05@gmail.com>
2024-07-16 16:16:14 +0200

    ✨ F02: ROUTER > create router & add orderPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import LoginPage from "./components/pages/login/LoginPage"
+import OrderPage from "./components/pages/order/OrderPage"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage/>
+  },
+  {
+    path: 'order',
+    element: <OrderPage/>
+  }
+])
 
 function App() {
-  return (
-      <LoginPage/>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
