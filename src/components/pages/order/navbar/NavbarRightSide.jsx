6e1b1fdcf51c1b714da47c44a5e7f613d7c46cbb
Johan <johan.perez05@gmail.com>
2024-07-25 17:08:43 +0200

    ✨ F06: ODER > NAVBARRIGHTSIDE> Add the toast and import its CSS and the icon

--- src/components/pages/order/navbar/NavbarRightSide.jsx
+++ src/components/pages/order/navbar/NavbarRightSide.jsx
@@ -1,11 +1,38 @@
 import styled from "styled-components";
 import Profile from "./Profile";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { theme } from "../../../../theme";
+import { FaUserSecret } from "react-icons/fa";
 
 export default function NavbarRightSide({ username }) {
+  
+  const onToggle = () => {
+    toast("Mode admin activé", {
+      icon: <FaUserSecret size={30} />,
+      theme: "dark",
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+
+  }
+  
   return (
     <NavbarRightSideStyled>
-        <ToggleButton 
+        <ToastContainer
+         
+        className="toaster" 
+        bodyClassName="body-toast" 
+        
+        />
+        <ToggleButton
+        onToggle={onToggle}
         labelIfUnchecked="Activer le mode admin"
         labelIfChecked="Désactiver le mode admin"
         />
@@ -17,4 +44,21 @@ export default function NavbarRightSide({ username }) {
 const NavbarRightSideStyled = styled.div`
     display: flex;
     align-items: center;
+    .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `;
