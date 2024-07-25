035ce1beef1d090456d80ceccbb7d1d3635d5aac
Johan <johan.perez05@gmail.com>
2024-07-25 16:48:06 +0200

    ✨ F06: ORDER>NAVBAR> Add the reusable-ui ToggleButton for the admin button

--- src/components/pages/order/navbar/NavbarRightSide.jsx
+++ src/components/pages/order/navbar/NavbarRightSide.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import Profile from "./Profile";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
 
 export default function NavbarRightSide({ username }) {
   return (
     <NavbarRightSideStyled>
-        {/* <div className="admin-button">Admin Button</div> */}
+        <ToggleButton 
+        labelIfUnchecked="Activer le mode admin"
+        labelIfChecked="Désactiver le mode admin"
+        />
         <Profile username={username}/>
     </NavbarRightSideStyled>
   )
