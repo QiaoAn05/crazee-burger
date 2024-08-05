4bb3132026548b55f6e6a7c6e12da4453a331022
Johan <johan.perez05@gmail.com>
2024-08-05 11:51:37 +0200

    💄 F07: ADMIN>Layout the panel admin

--- src/components/pages/order/main/Menu.jsx
+++ src/components/pages/order/main/Menu.jsx
@@ -11,24 +11,35 @@ export default function Menu() {
   //Render
   return (
     <MenuStyled>
-      {menu.map(product => (
-        <Product 
-        key={product.id} 
-        title={product.title}
-        imageSource={product.imageSource}
-        price={product.price}
-        />
-      ))}
+      <div className="product">
+        {menu.map(product => (
+          <Product 
+          key={product.id} 
+          title={product.title}
+          imageSource={product.imageSource}
+          price={product.price}
+          />
+        ))}
+      </div>
+      <div className="panel-admin">PanelAdmin</div>
     </MenuStyled>
   )
 }
 
 const MenuStyled = styled.section`
 background-color: ${theme.colors.background_white};
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 40px 30px;
-  padding: 50px 50px 150px;
-  justify-items: center;
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  .product {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 40px 30px;
+    padding: 50px 50px 150px;
+    justify-items: center;
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  }
+  .panel-admin {
+    background-color: red;
+    width: 100%;
+    position: sticky;
+    bottom: 0;
+  }
 `;
