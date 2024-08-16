3fb51a3203249dc6bee60891d7ed04e7046a0f79
Johan <johan.perez05@gmail.com>
2024-08-16 22:17:29 +0200

    💄 F08: MENU > Add EmptyMenuAdmin and EmptyMenuClient style

--- src/components/pages/order/main/menu/EmptyMenuAdmin.jsx
+++ src/components/pages/order/main/menu/EmptyMenuAdmin.jsx
@@ -1,12 +1,47 @@
 import styled from "styled-components";
+import { theme } from "../../../../../theme";
+import Button from "../../../../reusable-ui/Button";
 
 export default function EmptyMenuAdmin({ onReset }) {
   return (
     <EmptyMenuAdminStyled>
-      <span>Pas de produit</span>
-      <button onClick={onReset}>Générer de nouveaux produits</button>
+      <span className="title">Le menu est vide ?</span>
+      <span className="description">Cliquez ci-dessous le réinitialiser</span>
+      <Button Label={"Générer de nouveaux produits"} OnClick={onReset} />
     </EmptyMenuAdminStyled>
   );
 }
 
-const EmptyMenuAdminStyled = styled.div``;
+const EmptyMenuAdminStyled = styled.div`
+  background-color: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.strong};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 95vh;
+
+  .title,
+  .description {
+    text-align: center;
+    font-family: "Amatic SC", cursive;
+    color: ${theme.colors.greyBlue};
+  }
+
+  .title {
+    font-size: ${theme.fonts.sizes.P4};
+    font-weight: ${theme.fonts.weights.semiBold};
+  }
+
+  .description {
+    font-size: ${theme.fonts.sizes.P4};
+    margin-top: 20px;
+  }
+
+  button {
+    margin-top: 30px;
+    font-size: ${theme.fonts.sizes.XS};
+    width: auto;
+  }
+`;
