96294161f328c4c2d297a940175f2ff797d688d0
Johan <johan.perez05@gmail.com>
2024-08-21 15:40:09 +0200

    F09: ADMIN > EditForm > Retrieve info of Product clicked on with form from AddForm

--- src/components/pages/order/main/admin/adminPanel/EditForm.jsx
+++ src/components/pages/order/main/admin/adminPanel/EditForm.jsx
@@ -1,20 +1,68 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import OrderContext from "../../../../../../context/OrderContext";
 import HintMessage from "./HintMessage";
+import styled from "styled-components";
+import ImagePreview from "./ImagePreview";
+import TextInput from "../../../../../reusable-ui/TextInput";
+import { getInputTextConfig } from "./getInputTextConfig";
+import { EMPTY_PRODUCT } from "../../../../../../enums/product";
 
 export default function EditForm() {
-  const { productSelected } = useContext(OrderContext);
+  const { productSelected, setProductSelected } = useContext(OrderContext);
+  const { productBeingEdited, setProductBeingEdited } = useState(EMPTY_PRODUCT);
+
+  const inputTexts = getInputTextConfig(productSelected);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProductBeingEdited({ ...productBeingEdited, [name]: value });
+  };
 
   return (
-    <div>
-      <HintMessage />
-      {productSelected && (
-        <div>
-          <input type="text" value={productSelected.title} />
-          <input type="text" value={productSelected.imageSource} />
-          <input type="text" value={productSelected.price} />
-        </div>
-      )}
-    </div>
+    // <HintMessage />
+    <EditFormStyled>
+      <ImagePreview
+        imageSource={productSelected.imageSource}
+        title={productSelected.title}
+      />
+      <div className="input-fields">
+        {inputTexts.map((input) => (
+          <TextInput
+            key={input.id}
+            {...input}
+            onChange={handleChange}
+            version="minimalist"
+          />
+        ))}
+      </div>
+      {/* <div className="submit">{isSubmitted && <SubmitMessage />}</div> */}
+    </EditFormStyled>
   );
 }
+
+const EditFormStyled = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  height: 100%;
+  width: 70%;
+  grid-column-gap: 20px;
+  grid-row-gap: 8px;
+
+  .input-fields {
+    grid-area: 1/2/4/3;
+    display: grid;
+    grid-row-gap: 8px;
+  }
+  .submit {
+    grid-area: 4/2/5/-1;
+    display: flex;
+    align-items: center;
+    position: relative;
+    top: 3px;
+
+    .submit-button {
+      width: 50%;
+    }
+  }
+`;
