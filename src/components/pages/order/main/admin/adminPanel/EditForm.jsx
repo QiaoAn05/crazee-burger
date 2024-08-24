721618a42dd902b3f1786626b1ff3137045c62cb
Johan <johan.perez05@gmail.com>
2024-08-24 10:18:07 +0200

    💄 F09: ADMIN > EditForm > Add form footer message

--- src/components/pages/order/main/admin/adminPanel/EditForm.jsx
+++ src/components/pages/order/main/admin/adminPanel/EditForm.jsx
@@ -4,6 +4,7 @@ import ImagePreview from "./ImagePreview";
 import TextInput from "../../../../../reusable-ui/TextInput";
 import { getInputTextConfig } from "./getInputTextConfig";
 import { useContext } from "react";
+import { theme } from "../../../../../../theme";
 
 export default function EditForm() {
   //state
@@ -39,7 +40,12 @@ export default function EditForm() {
           />
         ))}
       </div>
-      {/* <div className="submit">{isSubmitted && <SubmitMessage />}</div> */}
+      <div className="submit">
+        <span className="sentence">
+          Cliquer sur un produit du menu pour le modifier{" "}
+          <span className="live-update">en temps réel</span>
+        </span>
+      </div>
     </EditFormStyled>
   );
 }
@@ -65,8 +71,12 @@ const EditFormStyled = styled.form`
     position: relative;
     top: 3px;
 
-    .submit-button {
-      width: 50%;
+    .sentence {
+      color: ${theme.colors.primary};
+      font-size: ${theme.fonts.sizes.SM};
+      .live-update {
+        text-decoration: underline;
+      }
     }
   }
 `;
