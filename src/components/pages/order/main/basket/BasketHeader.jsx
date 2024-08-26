f73a6c1bb9c2c02778a9a636a0b10b69bbfece89
Johan <johan.perez05@gmail.com>
2024-08-26 17:14:16 +0200

    💄F10: BASKET > BasketHeader > add static content style

--- src/components/pages/order/main/basket/BasketHeader.jsx
+++ src/components/pages/order/main/basket/BasketHeader.jsx
@@ -2,9 +2,37 @@ import React from "react";
 import styled from "styled-components";
 
 export default function BasketHeader() {
-  return <BasketHeaderStyled>BasketHeader</BasketHeaderStyled>;
+  return (
+    <BasketHeaderStyled>
+      <span className="total">Total</span>
+      <span className="price">0,00 €</span>
+    </BasketHeaderStyled>
+  );
 }
 
 const BasketHeaderStyled = styled.div`
-  background-color: blue;
+  background: #292729;
+  box-sizing: border-box;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 16px;
+  span {
+    font-family: Amatic SC;
+    font-size: 36px;
+    font-weight: 400;
+    line-height: 45.4px;
+    letter-spacing: 2px;
+    text-align: left;
+    color: #ffa01b;
+  }
+
+  .total {
+    font-weight: 400;
+  }
+
+  .price {
+    font-weight: 700;
+  }
 `;
