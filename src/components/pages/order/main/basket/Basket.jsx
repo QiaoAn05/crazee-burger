71b24dd96b5bbfb2fc60729dc81471ac8585bf0f
Johan <johan.perez05@gmail.com>
2024-08-27 14:49:06 +0200

    ♻️ F10: BASKET > Add BasketBody and clean code

--- src/components/pages/order/main/basket/Basket.jsx
+++ src/components/pages/order/main/basket/Basket.jsx
@@ -1,44 +1,23 @@
 import styled from "styled-components";
-import BasketHeader from "./BasketHeader";
-import BasketFooter from "./BasketFooter";
-import BasketOrder from "./BasketOrder";
 import { theme } from "../../../../../theme";
-import Header from "../../../../reusable-ui/Header";
 import Total from "./Total";
 import { formatPrice } from "../../../../../utils/maths";
 import Footer from "./Footer";
+import BasketBody from "./BasketBody";
 
 export default function Basket() {
   return (
     <BasketStyled>
-      {/* <BasketHeader />
-      <BasketOrder />
-      <BasketFooter /> */}
-
       <Total amountToPay={formatPrice(0)} />
-
-      <div className="body">Body</div>
+      <BasketBody />
       <Footer />
     </BasketStyled>
   );
 }
 
 const BasketStyled = styled.aside`
-  /* background: #f5f5f7;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  border-radius: 0px 0px 0px 15px;
-  width: 350px; */
-  /* correction Vi */
   background-color: pink;
   display: flex;
   flex-direction: column;
   box-shadow: ${theme.shadows.basket};
-
-  .body {
-    flex: 1;
-    background-color: ${theme.colors.background_white};
-    box-shadow: ${theme.shadows.basket};
-  }
 `;
