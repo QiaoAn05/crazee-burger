cd898f930dac73a4a47bea3f10f2c3c34e872dee
Johan <johan.perez05@gmail.com>
2024-08-27 15:34:59 +0200

    ♻️ F10: BASKET > HOOKS > Add useSuccessMessage.js

--- src/components/pages/order/main/admin/adminPanel/AddForm.jsx
+++ src/components/pages/order/main/admin/adminPanel/AddForm.jsx
@@ -1,13 +1,14 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import OrderContext from "../../../../../../context/OrderContext";
 import { EMPTY_PRODUCT } from "../../../../../../enums/product";
 import Form from "./Form";
 import SubmitButton from "./SubmitButton";
+import { useSuccessMessage } from "../../../../../../hooks/useSuccessMessage";
 
 export default function AddForm() {
   //state
   const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext);
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const { isSubmitted, displaySuccessMessage } = useSuccessMessage(5000);
 
   //comportements
   const handleSubmit = (e) => {
@@ -27,13 +28,6 @@ export default function AddForm() {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setNewProduct({ ...newProduct, [name]: value });
-  };
-
-  const displaySuccessMessage = () => {
-    setIsSubmitted(true);
-    setTimeout(() => {
-      setIsSubmitted(false);
-    }, 2000);
   };
 
   //affichage
