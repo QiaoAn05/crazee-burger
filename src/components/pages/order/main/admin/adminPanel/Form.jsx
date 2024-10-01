24d93c58eba2e4757d961ecffcf993e353f3e0ea
Johan <johan.perez05@gmail.com>
2024-10-01 16:13:07 +0200

    ♻️ F15: FORM > Reorganise grid system

--- src/components/pages/order/main/admin/adminPanel/Form.jsx
+++ src/components/pages/order/main/admin/adminPanel/Form.jsx
@@ -47,9 +47,25 @@ const FormStyled = styled.form`
   grid-row-gap: 8px;
 
   .input-fields {
+    background-color: blue;
     grid-area: 1/2/4/3;
     display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     grid-row-gap: 8px;
+
+    .title {
+      grid-area: 1/1/2/4;
+    }
+
+    .image-source {
+      grid-area: 2/1/3/4;
+    }
+
+    .price {
+      background-color: red;
+      grid-area: 3/1/4/2;
+    }
   }
   .form-footer {
     grid-area: 4/2/5/-1;
