749907486a37cc9f14b4783d3aac09ff5b0f1a9e
Johan <johan.perez05@gmail.com>
2024-10-01 17:38:26 +0200

    🚧 F15: FORM > Add reusable <selectInput/> (still ugly) in JSX

--- src/components/pages/order/main/admin/adminPanel/Form.jsx
+++ src/components/pages/order/main/admin/adminPanel/Form.jsx
@@ -3,6 +3,16 @@ import styled from "styled-components";
 import TextInput from "../../../../../reusable-ui/TextInput";
 import ImagePreview from "./ImagePreview";
 import { getInputTextConfig } from "./getInputTextConfig";
+import SelectInput from "../../../../../reusable-ui/SelectInput";
+
+const isAvailableOptions = [
+  { value: true, label: "En stock" },
+  { value: false, label: "En rupture" },
+];
+const isPublicisedOptions = [
+  { value: true, label: "Sans pub" },
+  { value: false, label: "Avec pub" },
+];
 
 const Form = React.forwardRef(
   ({ product, onSubmit, onChange, children, onFocus, onBlur }, ref) => {
@@ -28,14 +38,18 @@ const Form = React.forwardRef(
               onBlur={onBlur}
             />
           ))}
-          <select name="isAvailable" className="is-available" id="3">
-            <option value={true}>En stock</option>
-            <option value={false}>En rupture</option>
-          </select>
-          <select name="isPublicised" className="is-publicised" id="4">
-            <option value={true}>Sans pub</option>
-            <option value={false}>Avec pub</option>
-          </select>
+          <SelectInput
+            options={isAvailableOptions}
+            name={isAvailableOptions}
+            className="is-available"
+            id="3"
+          />
+          <SelectInput
+            options={isPublicisedOptions}
+            name={isPublicisedOptions}
+            className="is-publicised"
+            id="4"
+          />
         </div>
         <div className="form-footer">{children}</div>
       </FormStyled>
