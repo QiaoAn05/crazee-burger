d30ad6cc76df324bf405859c2a1d50de50deacda
Johan <johan.perez05@gmail.com>
2024-10-05 17:54:47 +0200

    ♻️ F15: FORM > Create Inputs to extract all TextInput and SelectInput

--- src/components/pages/order/main/admin/adminPanel/Form.jsx
+++ src/components/pages/order/main/admin/adminPanel/Form.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import TextInput from "../../../../../reusable-ui/TextInput";
 import ImagePreview from "./ImagePreview";
-import { getInputTextConfig, getSelectInputConfig } from "./getInputConfig";
-import SelectInput from "../../../../../reusable-ui/SelectInput";
+import { Inputs } from "./Inputs";
 
 const Form = React.forwardRef(
   ({ product, onSubmit, onChange, children, onFocus, onBlur }, ref) => {
@@ -11,35 +9,17 @@ const Form = React.forwardRef(
 
     //comportements
 
-    const inputTexts = getInputTextConfig(product);
-    const selectInputs = getSelectInputConfig(product);
-
     //affichage
     return (
       <FormStyled onSubmit={onSubmit}>
         <ImagePreview imageSource={product.imageSource} title={product.title} />
-        <div className="input-fields">
-          {inputTexts.map((input) => (
-            <TextInput
-              key={input.id}
-              {...input}
-              onChange={onChange}
-              version="minimalist"
-              ref={ref && input.name === "title" ? ref : null}
-              onFocus={onFocus}
-              onBlur={onBlur}
-            />
-          ))}
-          {selectInputs.map((selectInput) => (
-            <SelectInput
-              key={selectInput.id}
-              {...selectInput}
-              onChange={onChange}
-              onFocus={onFocus}
-              onBlur={onBlur}
-            />
-          ))}
-        </div>
+        <Inputs
+          product={product}
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          ref={ref}
+        />
         <div className="form-footer">{children}</div>
       </FormStyled>
     );
@@ -57,26 +37,6 @@ const FormStyled = styled.form`
   grid-column-gap: 20px;
   grid-row-gap: 8px;
 
-  .input-fields {
-    grid-area: 1/2/4/3;
-    display: grid;
-    grid-template-rows: repeat(3, 1fr);
-    grid-template-columns: repeat(3, 1fr);
-    grid-row-gap: 8px;
-    grid-column-gap: 8px;
-
-    .title {
-      grid-area: 1/1/2/4;
-    }
-
-    .image-source {
-      grid-area: 2/1/3/4;
-    }
-
-    .price {
-      grid-area: 3/1/4/2;
-    }
-  }
   .form-footer {
     grid-area: 4/2/5/-1;
     display: flex;
---
+++ src/components/pages/order/main/admin/adminPanel/Inputs.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { getInputTextConfig, getSelectInputConfig } from "./getInputConfig";
+import styled from "styled-components";
+import TextInput from "../../../../../reusable-ui/TextInput";
+import SelectInput from "../../../../../reusable-ui/SelectInput";
+
+export const Inputs = React.forwardRef(
+  ({ product, onChange, onFocus, onBlur }, ref) => {
+    const inputTexts = getInputTextConfig(product);
+    const selectInputs = getSelectInputConfig(product);
+
+    return (
+      <InputsStyled>
+        {inputTexts.map((input) => (
+          <TextInput
+            key={input.id}
+            {...input}
+            onChange={onChange}
+            version="minimalist"
+            ref={ref && input.name === "title" ? ref : null}
+            onFocus={onFocus}
+            onBlur={onBlur}
+          />
+        ))}
+        {selectInputs.map((selectInput) => (
+          <SelectInput
+            key={selectInput.id}
+            {...selectInput}
+            onChange={onChange}
+            onFocus={onFocus}
+            onBlur={onBlur}
+          />
+        ))}
+      </InputsStyled>
+    );
+  }
+);
+
+const InputsStyled = styled.div`
+  grid-area: 1/2/4/3;
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+  grid-row-gap: 8px;
+  grid-column-gap: 8px;
+
+  .title {
+    grid-area: 1/1/2/4;
+  }
+
+  .image-source {
+    grid-area: 2/1/3/4;
+  }
+
+  .price {
+    grid-area: 3/1/4/2;
+  }
+`;
