cdd0194094f1b395252724419fffeee607d9bf06
Johan <johan.perez05@gmail.com>
2024-12-03 14:49:43 +0100

    ♻️ TSE-02 > Reusable-UI > Header > Add HeaderType.ts

--- src/components/reusable-ui/Header.tsx
+++ src/components/reusable-ui/Header.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import styled from "styled-components";
 import { theme } from "../../theme";
+import { HeaderType } from "../../typescript/HeaderType";
 
-export default function Header({ children }) {
+export default function Header({ children }: HeaderType) {
   return <HeaderStyled>{children}</HeaderStyled>;
 }
 
