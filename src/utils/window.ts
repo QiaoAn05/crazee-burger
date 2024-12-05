38294489636e11b6b1c038902390ebf7376cd394
Johan <johan.perez05@gmail.com>
2024-12-05 09:49:06 +0100

    ♻️ TSE-03 > Utils > window > Add type

--- src/utils/window.ts
+++ src/utils/window.ts
@@ -1,15 +1,19 @@
 export const refreshPage = () => window.location.reload();
 
-export const setLocalStorage = (key, value) => {
+export const setLocalStorage = (key: string, value: any) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
-export const getLocalStorage = (key) => {
-  return JSON.parse(localStorage.getItem(key));
+export const getLocalStorage = <T = any>(key: string): T | null => {
+  const item = localStorage.getItem(key);
+  return item ? JSON.parse(item) : null;
 };
 
-export const deleteLocalStorage = (username, productToDelete) => {
-  const currentBasket = getLocalStorage(username) || [];
+export const deleteLocalStorage = <T extends { id: string }>(
+  username: string,
+  productToDelete: T
+): void => {
+  const currentBasket: T[] = getLocalStorage<T[]>(username) || [];
 
   // Filtrer le panier pour supprimer l'élément avec l'ID correspondant
   const updatedBasket = currentBasket.filter(
@@ -19,3 +23,4 @@ export const deleteLocalStorage = (username, productToDelete) => {
   // Mettre à jour le localStorage avec le panier modifié
   setLocalStorage(username, updatedBasket);
 };
+
