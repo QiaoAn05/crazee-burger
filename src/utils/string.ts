1f4f52c1dc0e9ba1401eb65ef5c3de228b8ec975
Johan <johan.perez05@gmail.com>
2024-12-05 17:00:19 +0100

    ♻️ TSE-03 > Utils > string > Add  boolean type

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,4 +1,4 @@
-export function convertStringToBoolean(input: string) {
+export function convertStringToBoolean(input: string | boolean) {
   if (typeof input === "boolean") return input
   return input === "true"
 }
